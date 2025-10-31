9555bfdf1f88adfba6cc9e1b9cc47a23758224b0
AgosLopez <solagos306@gmail.com>
2025-10-31 18:36:10 -0300

    subo GUI de pagos

---
+++ src/vista/FormPago.java
@@ -0,0 +1,11 @@
+package vista;
+
+import javax.swing.*;
+
+public class FormPago {
+    private JPanel panel1;
+    private JButton btnPaypal;
+    private JButton btnTarjeta;
+    private JLabel lblMensajePago;
+    private JPanel pnlPrincipal;
+}
